28badc68235a01b9328c03e11b0e583b3cecdbdd
Michael Tran <mtran45@gmail.com>
2015-07-03 14:58:37 +1000

    Add pan gesture to edit smiliness

--- Happiness/HappinessViewController.swift
+++ Happiness/HappinessViewController.swift
@@ -10,19 +10,37 @@ import UIKit
 
 class HappinessViewController: UIViewController, FaceViewDataSource {
     
-    
-    @IBOutlet weak var faceView: FaceView! {
-        didSet {
-            faceView.dataSource = self
-            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: "scale:"))
-        }
-    }
-    
     var happiness: Int = 100 { // 0 = very sad, 100 = ecstatic
         didSet {
             happiness = min(max(happiness, 0), 100)
             println("happiness = \(happiness)")
             updateUI()
+        }
+    }
+    
+    private struct Constants {
+        static let HappinessGestureScale: CGFloat = 4
+    }
+    
+    @IBAction func changeHappiness(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .Ended: fallthrough
+        case .Changed:
+            let translation = gesture.translationInView(faceView)
+            let happinessChange = -Int(translation.y / Constants.HappinessGestureScale)
+            if happinessChange != 0 {
+                happiness += happinessChange
+                gesture.setTranslation(CGPointZero, inView: faceView)
+            }
+        default: break
+        }
+    }
+    
+    
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            faceView.dataSource = self
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: "scale:"))
         }
     }
     
